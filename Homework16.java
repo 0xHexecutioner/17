9718e525f12fa492cc29a8f210dbaf45ba855f82
0xHexecutioner <116597049+0xHexecutioner@users.noreply.github.com>
2025-03-16 16:10:20 +0100

    Add files via upload

---
+++ Homework16.java
@@ -0,0 +1,12 @@
+package sample;
+import java.util.Scanner;
+
+public class main {  	
+	    public static void main(String[] args) {
+	    double pi = Math.PI;
+	    	        
+	    for (int i = 2; i <= 11; i++) 
+	    	System.out.printf("%." + i + "f\n", pi);
+	        }
+	    	    }
+
